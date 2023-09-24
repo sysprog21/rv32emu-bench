fcbdb06d89bf02a9f79734386a9013b5526a6b82
github-action-benchmark <github@users.noreply.github.com>
2023-09-24 13:28:50 +0000

    add Benchmarks (customBiggerIsBetter) benchmark result for 4dbfe8d9bcfd55119315affc3503d8fe80e199e1

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695561981487,
+  "lastUpdate": 1695562130698,
   "repoUrl": "https://github.com/sysprog21/rv32emu",
   "entries": {
     "Benchmarks": [
@@ -245,6 +245,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "Coremark",
             "value": 1132.121,
+            "unit": "Average iterations/sec over 10 runs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "qwe661234",
+            "username": "qwe661234"
+          },
+          "committer": {
+            "name": "qwe661234",
+            "username": "qwe661234"
+          },
+          "id": "4dbfe8d9bcfd55119315affc3503d8fe80e199e1",
+          "message": "Add a new fusion instruction and remove unnecessary ones ",
+          "timestamp": "2022-12-07T09:30:00Z",
+          "url": "https://github.com/sysprog21/rv32emu/pull/227/commits/4dbfe8d9bcfd55119315affc3503d8fe80e199e1"
+        },
+        "date": 1695562130177,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Dhrystone",
+            "value": 1274,
+            "unit": "Average DMIPS over 10 runs"
+          },
+          {
+            "name": "Coremark",
+            "value": 1133.984,
             "unit": "Average iterations/sec over 10 runs"
           }
         ]
