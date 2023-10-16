4d9a01e3f6d9fedb16405e655b0d4f7e9a5f4618
github-action-benchmark <github@users.noreply.github.com>
2023-10-16 13:14:12 +0000

    add Benchmarks (customBiggerIsBetter) benchmark result for d7f591f5a81e0b28efee0796449248919d693d6c

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1697461935619,
+  "lastUpdate": 1697462052676,
   "repoUrl": "https://github.com/sysprog21/rv32emu",
   "entries": {
     "Benchmarks": [
@@ -2387,6 +2387,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "Coremark",
             "value": 1248.264,
+            "unit": "Average iterations/sec over 10 runs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "qwe661234",
+            "username": "qwe661234"
+          },
+          "committer": {
+            "name": "qwe661234",
+            "username": "qwe661234"
+          },
+          "id": "d7f591f5a81e0b28efee0796449248919d693d6c",
+          "message": "Unify the hash function of cache and map",
+          "timestamp": "2022-12-07T09:30:00Z",
+          "url": "https://github.com/sysprog21/rv32emu/pull/249/commits/d7f591f5a81e0b28efee0796449248919d693d6c"
+        },
+        "date": 1697462052402,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Dhrystone",
+            "value": 1426.57,
+            "unit": "Average DMIPS over 10 runs"
+          },
+          {
+            "name": "Coremark",
+            "value": 1248.494,
             "unit": "Average iterations/sec over 10 runs"
           }
         ]
