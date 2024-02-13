92ad318cac0d5e0016e0473b7bbaaafc5bc5a07d
github-action-benchmark <github@users.noreply.github.com>
2024-02-13 04:22:32 +0000

    add Benchmarks (customBiggerIsBetter) benchmark result for 49ab1ace75aeb1d0dd6940cc91c29fd6db4a02dc

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707797979917,
+  "lastUpdate": 1707798152440,
   "repoUrl": "https://github.com/sysprog21/rv32emu",
   "entries": {
     "Benchmarks": [
@@ -8471,6 +8471,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "Coremark",
             "value": 1516.62,
+            "unit": "Average iterations/sec over 10 runs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "qwe661234",
+            "username": "qwe661234"
+          },
+          "committer": {
+            "name": "qwe661234",
+            "username": "qwe661234"
+          },
+          "id": "49ab1ace75aeb1d0dd6940cc91c29fd6db4a02dc",
+          "message": "Introducing register allocation for the tier-1 JIT compiler",
+          "timestamp": "2022-12-07T09:30:00Z",
+          "url": "https://github.com/sysprog21/rv32emu/pull/341/commits/49ab1ace75aeb1d0dd6940cc91c29fd6db4a02dc"
+        },
+        "date": 1707798152034,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Dhrystone",
+            "value": 1745.5,
+            "unit": "Average DMIPS over 10 runs"
+          },
+          {
+            "name": "Coremark",
+            "value": 1531.116,
             "unit": "Average iterations/sec over 10 runs"
           }
         ]
