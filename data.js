0489a9516cb58bfcc7e65c40043ed8b18d865279
github-action-benchmark <github@users.noreply.github.com>
2024-03-07 08:21:51 +0000

    add Benchmarks (customBiggerIsBetter) benchmark result for d58db3e24451b8418ec421a4a32cf07393bd6539

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1709799075534,
+  "lastUpdate": 1709799711308,
   "repoUrl": "https://github.com/sysprog21/rv32emu",
   "entries": {
     "Benchmarks": [
@@ -9327,6 +9327,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "Coremark",
             "value": 1470.904,
+            "unit": "Average iterations/sec over 10 runs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "henrybear327",
+            "username": "henrybear327"
+          },
+          "committer": {
+            "name": "henrybear327",
+            "username": "henrybear327"
+          },
+          "id": "d58db3e24451b8418ec421a4a32cf07393bd6539",
+          "message": "CI: Add Clang static analyzer",
+          "timestamp": "2023-11-11T09:23:44Z",
+          "url": "https://github.com/sysprog21/rv32emu/pull/361/commits/d58db3e24451b8418ec421a4a32cf07393bd6539"
+        },
+        "date": 1709799710942,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Dhrystone",
+            "value": 1687.5,
+            "unit": "Average DMIPS over 10 runs"
+          },
+          {
+            "name": "Coremark",
+            "value": 1478.909,
             "unit": "Average iterations/sec over 10 runs"
           }
         ]
