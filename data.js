a7a1161230fb9ac6b9506e37e24684e2bbc21502
github-action-benchmark <github@users.noreply.github.com>
2024-12-14 19:28:24 +0000

    add Benchmarks (customBiggerIsBetter) benchmark result for f315282275876d05cecbf7f825804d06d4121774

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1734201112814,
+  "lastUpdate": 1734204504491,
   "repoUrl": "https://github.com/sysprog21/rv32emu",
   "entries": {
     "Benchmarks": [
@@ -21105,6 +21105,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "Coremark",
             "value": 1422.377,
+            "unit": "Average iterations/sec over 10 runs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "ChinYikMing",
+            "username": "ChinYikMing"
+          },
+          "committer": {
+            "name": "ChinYikMing",
+            "username": "ChinYikMing"
+          },
+          "id": "f315282275876d05cecbf7f825804d06d4121774",
+          "message": "Bring up Linux kernel",
+          "timestamp": "2024-12-05T06:01:31Z",
+          "url": "https://github.com/sysprog21/rv32emu/pull/508/commits/f315282275876d05cecbf7f825804d06d4121774"
+        },
+        "date": 1734204503922,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Dhrystone",
+            "value": 1314,
+            "unit": "Average DMIPS over 10 runs"
+          },
+          {
+            "name": "Coremark",
+            "value": 1368.115,
             "unit": "Average iterations/sec over 10 runs"
           }
         ]
