14b221cd115a531674973fec17fa6670f5f09cb7
github-action-benchmark <github@users.noreply.github.com>
2025-01-26 15:00:44 +0000

    add Benchmarks (customBiggerIsBetter) benchmark result for 7eca0832abe0891aaee0ac019e85c3919917ee7d

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737903643097,
+  "lastUpdate": 1737903644954,
   "repoUrl": "https://github.com/sysprog21/rv32emu",
   "entries": {
     "Benchmarks": [
@@ -24293,6 +24293,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "Coremark",
             "value": 972.545,
+            "unit": "Average iterations/sec over 10 runs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "sysprog21",
+            "username": "sysprog21"
+          },
+          "committer": {
+            "name": "sysprog21",
+            "username": "sysprog21"
+          },
+          "id": "7eca0832abe0891aaee0ac019e85c3919917ee7d",
+          "message": "CI: Use newer Aarch64/Linux host",
+          "timestamp": "2025-01-24T14:05:36Z",
+          "url": "https://github.com/sysprog21/rv32emu/pull/548/commits/7eca0832abe0891aaee0ac019e85c3919917ee7d"
+        },
+        "date": 1737903643362,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Dhrystone",
+            "value": 1318,
+            "unit": "Average DMIPS over 10 runs"
+          },
+          {
+            "name": "Coremark",
+            "value": 961.276,
             "unit": "Average iterations/sec over 10 runs"
           }
         ]
