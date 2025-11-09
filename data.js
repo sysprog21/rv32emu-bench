c6047a3c51723825b4d2749be49d7468bcd0de3d
github-action-benchmark <github@users.noreply.github.com>
2025-11-09 08:26:52 +0000

    add Benchmarks (customBiggerIsBetter) benchmark result for d1beb5a0a262e44a2012ee697023580b9e2377f8

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762676192387,
+  "lastUpdate": 1762676812448,
   "repoUrl": "https://github.com/sysprog21/rv32emu",
   "entries": {
     "Benchmarks": [
@@ -33949,6 +33949,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "Coremark",
             "value": 918.918,
+            "unit": "Average iterations/sec over 10 runs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "sysprog21",
+            "username": "sysprog21"
+          },
+          "committer": {
+            "name": "sysprog21",
+            "username": "sysprog21"
+          },
+          "id": "d1beb5a0a262e44a2012ee697023580b9e2377f8",
+          "message": "Enable ThreadSanitizer across the entire multi-threaded JIT pipeline",
+          "timestamp": "2025-11-09T04:52:59Z",
+          "url": "https://github.com/sysprog21/rv32emu/pull/618/commits/d1beb5a0a262e44a2012ee697023580b9e2377f8"
+        },
+        "date": 1762676811259,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Dhrystone",
+            "value": 1281,
+            "unit": "Average DMIPS over 10 runs"
+          },
+          {
+            "name": "Coremark",
+            "value": 911.168,
             "unit": "Average iterations/sec over 10 runs"
           }
         ]
