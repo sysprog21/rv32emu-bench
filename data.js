ecc0d33e5fc42c1d92dd0698ab52fbb784801a59
github-action-benchmark <github@users.noreply.github.com>
2026-01-11 11:54:31 +0000

    add Benchmarks (customBiggerIsBetter) benchmark result for 3f9b18f5aecc25ce69f79095853d3ea91665289b

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768132440790,
+  "lastUpdate": 1768132471426,
   "repoUrl": "https://github.com/sysprog21/rv32emu",
   "entries": {
     "Benchmarks": [
@@ -44761,6 +44761,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "CoreMark",
             "value": 954.853,
+            "unit": "iterations/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "sysprog21",
+            "username": "sysprog21"
+          },
+          "committer": {
+            "name": "sysprog21",
+            "username": "sysprog21"
+          },
+          "id": "3f9b18f5aecc25ce69f79095853d3ea91665289b",
+          "message": "Enable T2C-based system emulation with thread-safe jit_cache",
+          "timestamp": "2026-01-11T09:52:19Z",
+          "url": "https://github.com/sysprog21/rv32emu/pull/695/commits/3f9b18f5aecc25ce69f79095853d3ea91665289b"
+        },
+        "date": 1768132469333,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Dhrystone",
+            "value": 1600.667,
+            "unit": "DMIPS"
+          },
+          {
+            "name": "CoreMark",
+            "value": 934.126,
             "unit": "iterations/sec"
           }
         ]
