5600d7e8b477fa0cb496c8dea15c4d4d641d87cd
github-action-benchmark <github@users.noreply.github.com>
2026-08-02 11:21:55 +0000

    add Benchmarks (customBiggerIsBetter) benchmark result for 67316d0bd5dc9f1b1a16d2807cae1c0cc76b0459

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785582191166,
+  "lastUpdate": 1785669715633,
   "repoUrl": "https://github.com/sysprog21/rv32emu",
   "entries": {
     "Benchmarks": [
@@ -49361,6 +49361,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "CoreMark",
             "value": 1438.281,
+            "unit": "iterations/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "ChinYikMing",
+            "username": "ChinYikMing"
+          },
+          "committer": {
+            "name": "ChinYikMing",
+            "username": "ChinYikMing"
+          },
+          "id": "67316d0bd5dc9f1b1a16d2807cae1c0cc76b0459",
+          "message": "Fix system emulation reboot",
+          "timestamp": "2026-07-30T15:20:40Z",
+          "url": "https://github.com/sysprog21/rv32emu/pull/638/commits/67316d0bd5dc9f1b1a16d2807cae1c0cc76b0459"
+        },
+        "date": 1785669713452,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Dhrystone",
+            "value": 1556,
+            "unit": "DMIPS"
+          },
+          {
+            "name": "CoreMark",
+            "value": 1114.369,
             "unit": "iterations/sec"
           }
         ]
